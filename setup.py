42826536357dc6c95ec44173c0b65b997f7bcbdb
Redoffer <145905245+Redoffer@users.noreply.github.com>
2024-04-03 11:43:11 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
+import setuptools
 
+setuptools.setup(
+  include_package_data=True,
+  name='dbwjdgus15',
+  version='0.0.1',
+  description= 'oss-dev my calculator 0001',
+  author= 'junghyun',
+  author_email= 'dbwjdgus15@naver.com',
+  url = "https://github.com/Redoffer/calc1000", 
+  download_url = "https://github.com/Redoffer/calc0001/archive/refs,/tags/v0.0.1.zip", 
+  install_requires=['pytest'], 
+  long_description = 'oss-dev calculator python module', 
+  long_description_content_type = 'text/markdown', 
+  classifiers=[ 
+    "Programming Language Python :: 3", 
+    "Operating System :: OS Independent",
+    ],
+)
